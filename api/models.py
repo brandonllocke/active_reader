04d20852214077352785b5780989a02dfe44a075
Brandon Locke <git@brandonllocke.com>
2022-07-22 20:48:28 -0400

    prevent duplicate titles

--- api/models.py
+++ api/models.py
@@ -2,11 +2,15 @@ from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+import datetime
+
 
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=300, null=False)
+    title = models.CharField(max_length=300, null=False, unique=True)
     active = models.BooleanField(default=True, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
 
 class User(AbstractUser):
@@ -16,5 +20,5 @@ class User(AbstractUser):
 class Reading(models.Model):
     id = models.AutoField(primary_key=True)
     book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
-    time = models.DateTimeField(null=False)
+    time = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
