61ac93e5542fb1f332f30cf06ed19b4d8040adb9
Brandon Locke <git@brandonllocke.com>
2022-07-24 15:54:28 -0400

    removed secret key from settings though it was only the insecure one

--- api/tests.py
+++ api/tests.py
@@ -16,9 +16,7 @@ class BookTestCase(TestCase):
     TITLE = 'title'
 
     def test_list_none(self):
-        client = APIClient()
-        response = client.get(self.API_ENDPOINT)
-        content = json.loads(response.content)
+        content = self._get_books()
         self._assertSuccess(content)
         self.assertEquals(len(content[self.DATA]), 0)
 
@@ -27,62 +25,56 @@ class BookTestCase(TestCase):
 
     def test_list_one(self):
         self._successfully_add_book(self.PRIMARY_TITLE)
-        client = APIClient()
-        response = client.get(self.API_ENDPOINT)
-        content = json.loads(response.content)
+        content = self._get_books()
         self._assertSuccess(content)
         self.assertEquals(len(content[self.DATA]), 1)
 
     def test_edit_active(self):
         self._successfully_add_book(self.PRIMARY_TITLE)
-        client = APIClient()
         data = {self.ACTIVE: False}
-        response = client.post(f'{self.API_ENDPOINT}1', data)
-        content = json.loads(response.content)
+        content = self._edit_first_book(data)
         self._assertSuccess(content)
         self._assertTitle(content, self.PRIMARY_TITLE)
         self._assertActive(content, False)
 
     def test_add_two_of_same_title(self):
         self._successfully_add_book(self.PRIMARY_TITLE)
-        client = APIClient()
-        data = {self.TITLE: self.PRIMARY_TITLE}
-        response = client.post(self.API_ENDPOINT, data)
-        content = json.loads(response.content)
+        content = self._add_book(self.PRIMARY_TITLE)
         self._assertFailure(content)
+        self._assertValidationErrorMessage(content, self.TITLE, "A book by this title already exists.")
 
     def test_add_two_books(self):
         self._successfully_add_book(self.PRIMARY_TITLE)
         self._successfully_add_book(self.SECONDARY_TITLE)
-        client = APIClient()
-        response = client.get(self.API_ENDPOINT)
-        content = json.loads(response.content)
+        content = self._get_books()
         self._assertSuccess(content)
         self.assertEquals(len(content[self.DATA]), 2)
 
     def test_change_title_fails(self):
         self._successfully_add_book(self.PRIMARY_TITLE)
-        client = APIClient()
         data = {self.TITLE: self.SECONDARY_TITLE}
-        response = client.post(f'{self.API_ENDPOINT}1', data)
-        content = json.loads(response.content)
+        content = self._edit_first_book(data)
         self._assertFailure(content)
         self._assertValidationErrorMessage(content, self.TITLE, "Cannot update book title after creation.")
 
     def _successfully_add_book(self, title):
         content = self._add_book(title)
-        self._assertBookAdded(content, title)
-
-    def _add_book(self, title):
-        client = APIClient()
-        data = {self.TITLE: title}
-        response = client.post(self.API_ENDPOINT, data)
-        return json.loads(response.content)
-
-    def _assertBookAdded(self, content, title):
         self._assertSuccess(content)
         self._assertTitle(content, title)
         self._assertActive(content, True)
+
+    def _add_book(self, title):
+        data = {self.TITLE: title}
+        response = APIClient().post(self.API_ENDPOINT, data)
+        return json.loads(response.content)
+
+    def _edit_first_book(self, data):
+        response = APIClient().post(f'{self.API_ENDPOINT}1', data)
+        return json.loads(response.content)
+
+    def _get_books(self):
+        response = APIClient().get(self.API_ENDPOINT)
+        return json.loads(response.content)
 
     def _assertSuccess(self, content):
         self.assertEqual(content[self.STATUS], self.SUCCESS, "Did not receive success status")
